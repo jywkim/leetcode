4a3df3f6a275bd4ad93d9e35c686573ddec82732
jywkim <jkim589@gmail.com>
2022-02-13 18:08:20 -0500

    fizz buzz

---
+++ javascript/fizzBuzz.js
@@ -0,0 +1,15 @@
+var fizzBuzz = function(n) {
+    let i = 1;
+    let array = [];
+    for (i; i <= n; i++) {
+        let newString = "";
+        if (i % 3 === 0) {
+            newString += "Fizz";
+        }
+        if (i % 5 === 0) {
+            newString += "Buzz";
+        }
+        array.push(newString.length ? newString : i.toString());
+    }
+    return array;
+};
