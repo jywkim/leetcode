06cb874294dcd34276f25e649b7f8c79451f0b10
jywkim <jkim589@gmail.com>
2022-02-15 20:12:59 -0500

    valid parentheses: fixed array.push()

--- javascript/validParentheses.js
+++ javascript/validParentheses.js
@@ -1,15 +1,4 @@
 var isValid = function(s) {
-    /*
-    let len = s.length;
-    for (let i = 0; i < len; i++) {
-        if ((s[i] === "(" && s[i + 1] !== ")") ||
-            (s[i] === "{" && s[i + 1] !== "}") ||
-            (s[i] === "[" && s[i + 1] !== "]")) {
-                return false;
-        }
-    }
-    return true;
-    */
     let array = [];
     let len = s.length;
     let map = {
@@ -21,7 +10,7 @@ var isValid = function(s) {
         if (array.length > 0 && s[i] === map[array[array.length - 1]]) {
             array.pop();
         } else {
-            array.push[s[i]];
+            array.push(s[i]);
         }
     }
     return array.length === 0;
