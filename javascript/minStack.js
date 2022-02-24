88f523857ff059651b0c7caac3d0c5d3c49e7df2
jywkim <jkim589@gmail.com>
2022-02-23 19:31:18 -0500

    min stack

---
+++ javascript/minStack.js
@@ -0,0 +1,26 @@
+var MinStack = function() {
+    this.min = [];
+    this.stack = [];
+};
+
+MinStack.prototype.push = function(val) {
+    if (this.min.length === 0 || val <= this.min[this.min.length - 1]) {
+        this.min.push(val);
+    }
+    this.stack.push(val);
+};
+
+MinStack.prototype.pop = function() {
+    let val = this.stack.pop();
+    if (val === this.min[this.min.length - 1]) {
+        this.min.pop();
+    }
+};
+
+MinStack.prototype.top = function() {
+    return this.stack.length ? this.stack[this.stack.length - 1] : 0;
+};
+
+MinStack.prototype.getMin = function() {
+    return this.min.length ? this.min[this.min.length - 1] : 0;
+};
