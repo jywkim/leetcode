828ae0644845dda258a3fd45436a8ed44a8e9032
jywkim <jkim589@gmail.com>
2022-02-24 20:54:54 -0500

    implement strStr()

---
+++ javascript/implementStrStr().js
@@ -0,0 +1,13 @@
+var strStr = function(haystack, needle) {
+    if (!needle) return 0;
+    if (!haystack || needle.length > haystack.length) return -1;
+
+    for (let i = 0; i < haystack.length; i++) {
+       let string = haystack.substr(i, needle.length);
+       if (string === needle) {
+           return i;
+       }
+    }
+
+    return -1;
+};
