e1755ffc6e02da298d5880cddf62b1fbcd50d89f
jywkim <jkim589@gmail.com>
2022-02-25 19:09:04 -0500

    climbing stairs

---
+++ javascript/climbingStairs.js
@@ -0,0 +1,10 @@
+var climbStairs = function(n) {
+    if (n <= 1) return 1;
+    //return climbStairs(n - 1) + climbStairs(n - 2);
+    
+    let steps = [1, 1];
+	for (let i = 2; i <= n; i++) {
+		steps.push(steps[i - 1] + steps[i - 2]);
+	}
+	return steps[n];
+};
