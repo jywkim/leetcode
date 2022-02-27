1f81aae14f9e969e7025542b4a48475dd98ce189
jywkim <jkim589@gmail.com>
2022-02-27 18:33:11 -0500

    reverse words in a string III

---
+++ javascript/reverseWordsInAStringIII.js
@@ -0,0 +1,14 @@
+var reverseWords = function(s) {
+    return s.split(" ").map((word) => {
+        s = word.split("");
+        let l = 0, r = word.length - 1;
+        while (l < r) {
+            let temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+        return s.join("");
+    }).join(" ");
+};
