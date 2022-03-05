0d3b6f66ed24bc420304ed0a39cc4f2fa38365af
jywkim <jkim589@gmail.com>
2022-03-04 20:16:05 -0500

    symmetric tree

---
+++ javascript/symmetricTree.js
@@ -0,0 +1,9 @@
+var isSymmetric = function(root) {
+    return symmetric(root, root)
+};
+
+function symmetric(l, r) {
+    if (l === null && r === null) return true;
+    if (l === null || r === null) return false;
+    return (l.val === r.val) && symmetric(l.left, r.right) && symmetric(l.right, r.left);
+}
