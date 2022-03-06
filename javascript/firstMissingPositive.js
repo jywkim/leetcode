92ac4c37d69bdde7ce29c99ce07a811784211d68
jywkim <jkim589@gmail.com>
2022-03-06 16:29:23 -0500

    first missing positive

---
+++ javascript/firstMissingPositive.js
@@ -0,0 +1,23 @@
+var firstMissingPositive = function(nums) {
+    let len = nums.length;
+    let i = 0;
+    while (i < len) {
+        if (nums[i] > 0 &&
+            nums[i] !== i + 1 &&
+            nums[i] !== nums[nums[i] - 1]) {
+            swap(nums, i, nums[i] - 1)
+        } else {
+            i++;
+        }
+    }
+    for (let j = 0; j < len; j++) {
+        if (nums[j] !== j + 1) return j + 1;
+    }
+    return len + 1;
+};
+
+function swap(nums, i , j) {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
